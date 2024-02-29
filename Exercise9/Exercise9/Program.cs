2d4a62e7131f14c5b1438d78920b25d2004a71a3
Yolo518 <145461025+Yolo518@users.noreply.github.com>
2024-02-29 10:20:15 -0500

    Part 1 working on

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Makeng rows and colums of *
+
+            Console.WriteLine("How many rows do you want");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("How wide do you want each row to be");
+            int with = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= with; i ++ )
+            {
+                Console.Write("*");
+                for (int j = 1; j <= row; j++)
+                {
+                    Console.WriteLine("*");
+                }
+            }
+        }
+    }
+}
